30598b218d904d7df5bb2f06228b01e2c7adbd5f
Ponty <marcelino.ponty@gmail.com>
2018-10-07 23:32:01 +0700

    implemented destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -41,4 +41,14 @@ def show
 	@portfolio_item = Portfolio.find(params[:id])
 end
 
+def destroy
+	@portfolio_item = Portfolio.find(params[:id])
+
+	@portfolio_item.destroy
+
+	respond_to do |format|
+		format.html {redirect_to portfolios_url, notice: 'Portfolios was removed'}
+	end
+end
+
 end
