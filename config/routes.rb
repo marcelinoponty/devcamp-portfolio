2e9254a246971b87e031534d2f37f38912f105b5
Ponty <marcelino.ponty@gmail.com>
2018-10-08 22:27:52 +0700

    customize portfolio show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]  
   #get 'pages/home'
   #get 'pages/about'
   #get 'pages/contact'
+
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
