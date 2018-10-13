b1fea4b65fce248616250bfc1e94ae34624d50ce
Ponty <marcelino.ponty@gmail.com>
2018-10-14 00:03:44 +0700

    refactor portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -17,7 +17,7 @@ end
 
   def create
     #@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
@@ -36,7 +36,7 @@ end
   	@portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
       else
         format.html { render :edit }
@@ -56,6 +56,12 @@ def destroy
 	respond_to do |format|
 		format.html {redirect_to portfolios_url, notice: 'Portfolios was removed'}
 	end
+end
+
+private
+
+def portfolio_params
+  params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
 end
 
 end
